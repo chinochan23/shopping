cbf0ff5b1bf7f9ce9e02b644c04855c73691c331
stan <ljystan@gmail.com>
2019-08-26 19:19:33 +0800

    初始化项目2

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // '@': 'src' ，默认已经配置
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+      }
+    }
+  }
+}
